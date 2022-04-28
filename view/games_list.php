cbaf6f789b842d5fa7395e7f6edccfc43266e206
Lawrence De Belen <ltdebelen@gmail.com>
2022-04-27 21:48:39 -0500

    Add Game Functionality | Add modal for new game form

--- view/games_list.php
+++ view/games_list.php
@@ -2,6 +2,32 @@
 
 <div class="py-5">
     <div class="container">
+        <div class="d-flex justify-content-end">
+            <button type="button" class="btn btn-primary pull-right" data-bs-toggle="modal" data-bs-target="#addGameModal">
+                Add Game
+            </button>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addGameModal" tabindex="-1" aria-labelledby="addGameModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="addGameModalLabel">Add Game</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="." method="post">
+                            <div class="mb-3">
+                                <label for="gameNameInput" class="form-label">Game Name</label>
+                                <input type="hidden" name="action" value="add_game">
+                                <input type="text" class="form-control" id="gameNameInput" name="game_name" placeholder="Enter Game Name..." required>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         <table class="table">
             <thead>
                 <tr>
