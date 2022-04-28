b8234cb4260b8d52e28f1370945cb14d19d217e3
Lawrence De Belen <ltdebelen@gmail.com>
2022-04-27 20:49:42 -0500

    Requesting for API Data | Adds Bootstrap Table

--- model/api.php
+++ model/api.php
@@ -2,5 +2,18 @@
 
 function get_games()
 {
-    return 'gameslist test';
+    $url = 'https://codechallenge.essensedesigns.info/games/list';
+
+    $curl = curl_init($url);
+
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $server_response = curl_exec($curl);
+
+    curl_close($curl);
+
+    $server_response = json_decode($server_response, true);
+
+    return $server_response;
 }
